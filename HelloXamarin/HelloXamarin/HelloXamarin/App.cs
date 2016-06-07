64c8cc256b9e2bfb604fcf516b5471b98238ce84
Mark Allibone <mallibone@outlook.com>
2016-06-07 20:28:38 +0200

    Adds DevDays view

--- HelloXamarin/HelloXamarin/HelloXamarin/App.cs
+++ HelloXamarin/HelloXamarin/HelloXamarin/App.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using HelloXamarin.Views;
 using Xamarin.Forms;
 
 namespace HelloXamarin
@@ -12,19 +12,7 @@ namespace HelloXamarin
         public App()
         {
             // The root page of your application
-            MainPage = new ContentPage
-            {
-                Content = new StackLayout
-                {
-                    VerticalOptions = LayoutOptions.Center,
-                    Children = {
-                        new Label {
-                            XAlign = TextAlignment.Center,
-                            Text = "Welcome to Xamarin Forms!"
-                        }
-                    }
-                }
-            };
+            MainPage = new MainView();
         }
 
         protected override void OnStart()
