73d5cddc4c02af7f55f9ac1ee7beba90da147cab
Mark Allibone <mallibone@outlook.com>
2016-06-08 18:13:31 +0200

    Adds view with webview

--- HelloXamarinWeb/HelloXamarinWeb/HelloXamarinWeb/App.cs
+++ HelloXamarinWeb/HelloXamarinWeb/HelloXamarinWeb/App.cs
@@ -12,19 +12,7 @@ namespace HelloXamarinWeb
         public App()
         {
             // The root page of your application
-            MainPage = new ContentPage
-            {
-                Content = new StackLayout
-                {
-                    VerticalOptions = LayoutOptions.Center,
-                    Children = {
-                        new Label {
-                            XAlign = TextAlignment.Center,
-                            Text = "Welcome to Xamarin Forms!"
-                        }
-                    }
-                }
-            };
+            MainPage = new MainView();
         }
 
         protected override void OnStart()
